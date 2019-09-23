025fc7864db934426b6d21884a3a45ff2c0fda5a
Fourza0012 <kittichat.tun@gmail.com>
2019-09-23 17:48:45 +0700

    Change CharacterCard and WordCard

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import CharacterCard from './CharacterCard';
 import './App.css';
-export default class WordCard extends
-    Component {
+export default class WordCard extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            attempt: 1
+        }
+    }
+    activationHandler = c => { console.log(`${c} has been activated.`) }
     render() {
         return (
             <div>
-                {Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i} />)}
+                {Array.from(this.props.value).map((c, i) =>
+                    <CharacterCard value={c} key={i} attempt={this.state.attempt} activationHandler={this.activationHandler} />)}
             </div>
         );
     }
