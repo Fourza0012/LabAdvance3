a8f2c1af7d91da7a5a9e9074b064c799321ca4d6
Fourza0012 <kittichat.tun@gmail.com>
2019-09-23 17:42:19 +0700

    Add WordCard

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
+const word = "Hello";
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <CharacterCard value="h" />
-                <CharacterCard value="i" />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="card">
+        <WordCard value="hello" />
+      </div>
+    );
+  }
 }
+
 export default App;
